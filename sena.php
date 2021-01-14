c90de3f8bbb72dd56ad2c908eacc8c5bf231ee9e
angie3005 <68611848+angie3005@users.noreply.github.com>
2021-01-14 15:15:01 -0500

    Create sena.php

---
+++ sena.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html lang="es">
+   <head>
+    <meta charset="UTF-8">    
+  		<link rel="shortcut icon" type="image/x-icon" href="imagenes/bancoop.png">
+        <link href="css/main.css" rel="stylessheet">
+  		<title>BANCOOP</title>
+  </head>
+  </html>
+  
